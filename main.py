d8efc2e05c45a14a7490145764099b698c94818d
abdallahal-sarayrah <abdallah.dev85@gmail.com>
2022-10-27 12:06:05 +0300

    save application

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -14,14 +16,25 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.get(url)
 
-sign_btn = driver.find_element(By.CSS_SELECTOR,'a.nav__button-secondary.btn-md.btn-secondary-emphasis')
+sign_btn = driver.find_element(By.CSS_SELECTOR, 'a.nav__button-secondary.btn-md.btn-secondary-emphasis')
 sign_btn.click()
 
-email_txt = driver.find_element(By.ID,'username')
+email_txt = driver.find_element(By.ID, 'username')
 email_txt.send_keys(email)
 
-password_txt = driver.find_element(By.ID,'password')
+password_txt = driver.find_element(By.ID, 'password')
 password_txt.send_keys(password)
 
-login_btn = driver.find_element(By.CSS_SELECTOR,"button.btn__primary--large.from__button--floating")
+login_btn = driver.find_element(By.CSS_SELECTOR, "button.btn__primary--large.from__button--floating")
 login_btn.click()
+
+corp_links = driver.find_elements(By.CSS_SELECTOR,
+                                  "a.disabled.ember-view.job-card-container__link.job-card-list__title")
+
+for index in range(5):
+    time.sleep(0.5)
+    corp_links[index].click()
+    time.sleep(0.5)
+    save_btn = driver.find_element(By.CSS_SELECTOR,
+                                   'button.jobs-save-button.artdeco-button.artdeco-button--3.artdeco-button--secondary')
+    save_btn.click()
